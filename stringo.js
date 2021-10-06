0c457309704fdadca3284475255cc13b4d6c3b46
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-06 14:27:34 +0530

    Import all the functions

--- stringo.js
+++ stringo.js
@@ -1,6 +1,18 @@
+const changeCase = require('./src/changeCase.js');
+const chunk = require('./src/chunk.js');
+const clear = require('./src/clear.js');
+const distance = require('./src/distance.js');
+const has = require('./src/has.js');
+const removeWhitespace = require('./src/removeWhitespace.js');
+
 class Stringo{
     constructor(){
-
+        this.changeCase = changeCase;
+        this.chunk = chunk;
+        this.clear = clear;
+        this.distance = distance;
+        this.has = has;
+        this.removeWhitespace = removeWhitespace;
     }
 }
 
