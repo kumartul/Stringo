547ecb3610c6a44cc7fa1c489510ebaf6b0233a9
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-06 16:01:28 +0530

    Update stringo.js

--- stringo.js
+++ stringo.js
@@ -4,6 +4,9 @@ const clear = require('./src/clear.js');
 const distance = require('./src/distance.js');
 const has = require('./src/has.js');
 const removeWhitespace = require('./src/removeWhitespace.js');
+const reverse = require('./src/reverse.js');
+const sort = require('./src/sort.js');
+const words = require('./src/words.js');
 
 class Stringo{
     constructor(){
@@ -13,6 +16,10 @@ class Stringo{
         this.distance = distance;
         this.has = has;
         this.removeWhitespace = removeWhitespace;
+        this.reverse = reverse;
+        this.sortChars = sort.sortChars;
+        this.sortWords = sort.sortWords;
+        this.words = words;
     }
 }
 
