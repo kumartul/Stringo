92e94636cfe1d2829a7849fdfd599a9207e27c86
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-06 14:27:59 +0530

    Write a function to change the case of th string

---
+++ src/changeCase.js
@@ -0,0 +1,24 @@
+const isStr = require('./utils/isStr.js');
+
+// Function: Changes the case of the string
+function changeCase(string, stringCase){
+    if(isStr(string)){
+        switch(stringCase){
+            case "upper":
+                return string.toUpperCase();
+            case "lower":
+                return string.toLowerCase();
+            case "sentence":
+                let firstChar = string[0].toUpperCase();
+                let remainingChars = string.slice(1,).toLowerCase();
+                return firstChar + remainingChars;
+            default: 
+                throw new RangeError("Failed to execute 'changeCase': Invalid case");
+        }
+    }
+    else{
+        throw new TypeError("Failed to execute 'changeCase': First argument must be a string");
+    }
+}
+
+module.exports = changeCase;
