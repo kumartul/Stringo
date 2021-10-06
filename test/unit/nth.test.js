53b7781f19ace227abfb58bf444ebadb235272ef
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-06 16:51:05 +0530

    Successfully tested nth()

---
+++ test/unit/nth.test.js
@@ -0,0 +1,9 @@
+const nth = require('../../src/nth.js');
+
+test('returns the character at the nth position of the string', () => {
+    expect(nth("Foobar", 3)).toBe("b");
+});
+
+test('returns the character at the nth position of the string', () => {
+    expect(nth("Foobar", -2)).toBe("a");
+});
