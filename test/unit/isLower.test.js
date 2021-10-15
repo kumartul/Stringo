51becc1baa1b9349cbbd0f4e65b78c2ea26f9bd7
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-15 07:53:35 +0530

    Successfully tested isLower

---
+++ test/unit/isLower.test.js
@@ -0,0 +1,9 @@
+const isLower = require('../../src/isLower.js');
+
+test('checks whether the string is in lowercase or not', () => {
+	expect(isLower("foobar")).toBe(true);
+});
+
+test('checks whether the string is in lowercase or not', () => {
+	expect(isLower("Foobar")).toBe(false);
+});
