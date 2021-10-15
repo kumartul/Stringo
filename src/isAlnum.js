139408ae10542cbe32dc2e4dd70a933898575c98
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-15 07:33:55 +0530

    Change the parameter name to 'string' from 'input'

--- src/isAlnum.js
+++ src/isAlnum.js
@@ -2,9 +2,9 @@ const validator = require('validator');
 const isStr = require('./utils/isStr.js');
 
 // Checks if the string is an alphanumeric string or not
-function isAlnum(input){
-	if(isStr(input)){
-		return validator.default.isAlphanumeric(input);
+function isAlnum(string){
+	if(isStr(string)){
+		return validator.default.isAlphanumeric(string);
 	}
 	else{
 		throw new TypeError("Failed to execute 'isAlnum': Argument must be a string");
