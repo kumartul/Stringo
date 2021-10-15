83f7f079c36fa790212911d51319b38940bdd240
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-15 07:48:57 +0530

    Successfully tested isAlnum

---
+++ test/unit/isAlnum.test.js
@@ -0,0 +1,9 @@
+const isAlnum = require('../../src/isAlnum.js');
+
+test('checks whether the string is alphanumeric or not', () => {
+	expect(isAlnum("Foo bar")).toBe(false);
+});
+
+test('checks whether the string is alphanumeric or not', () => {
+	expect(isAlnum("Foobar")).toBe(true);
+});
