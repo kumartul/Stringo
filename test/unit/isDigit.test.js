827c6f48ca6bf30cf0d6a1ceb66e4cdb96118e41
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-15 07:52:19 +0530

    Successfully tested isDigit

---
+++ test/unit/isDigit.test.js
@@ -0,0 +1,9 @@
+const isDigit = require('../../src/isDigit.js');
+
+test('checks whether all the characters of a string are digits or not', () => {
+	expect(isDigit("8234089")).toBe(true);
+});
+
+test('checks whether all the characters of a string are digits or not', () => {
+	expect(isDigit("foo8234wow089bar")).toBe(false);
+});
