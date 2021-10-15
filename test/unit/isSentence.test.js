a1c635f089dfb7f6f9ba4f61436690f24f5eefa7
Atul Kumar <akumarofficial4224@gmail.com>
2021-10-15 07:56:15 +0530

    Successfully tested isSentence

---
+++ test/unit/isSentence.test.js
@@ -0,0 +1,9 @@
+const isSentence = require('../../src/isSentence.js');
+
+test('checks if the string is in sentence case or not', () => {
+	expect(isSentence("Foobar")).toBe(true);
+});
+
+test('checks if the string is in sentence case or not', () => {
+	expect(isSentence("foobar")).toBe(false);
+});
